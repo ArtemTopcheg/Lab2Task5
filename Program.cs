111c3e4505b7c675b2b0254020cdcfda91033726
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:34:59 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Task5
+int[] nums = new int[5];
+int count = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    int iElement = Convert.ToInt32(Console.ReadLine());
+    nums[i] = iElement;
+
+    if (iElement < 0)
+    {
+        continue;
+    }
+    else
+    {
+        nums[count] = iElement;
+        count++;
+
+    }
+}
+Console.WriteLine("Итоговый массив");
+for (int i = 0; i < count; i++)
+{
+    Console.Write(nums[i] + " ");
+}
